7c8c210a4a95180881141051cfed90eb38d15fcb
Damian Lowe <damian.lowe@netreveal.ai>
2023-05-14 16:14:48 +0100

    Add first impl for top_3_words function

--- src/main/scala/com/industries/damian/Main.scala
+++ src/main/scala/com/industries/damian/Main.scala
@@ -1,7 +1,17 @@
 package com.industries.damian
 
 object Main {
-  def top_3_words(s: String): List[String] = {
-    List.empty
+  def top_3_words(str: String): List[String] = {
+    str.split("[^a-zA-Z0-9']")
+      .withFilter(word => word != "")
+      .map(word => word.toLowerCase)
+      .foldLeft(Map[String, Int]()) { (acc, word) =>
+        acc.get(word) match
+          case Some(freq) => acc.updated(word, freq + 1)
+          case None => acc.updated(word, 1)
+      }.toList
+      .sortWith((entry1, entry2) => entry1._2 > entry2._2)
+      .take(3)
+      .map(entry => entry._1)
   }
 }
