be0af3e341bbb9f138e856f9a79b52c7d444a2bf
Damian Lowe <damian.lowe@netreveal.ai>
2023-05-21 17:20:50 +0100

    Add second impl for top_3_words function, more complicated, but with attempted efficiency gains (efficiency not tested)

--- src/main/scala/com/industries/damian/Main.scala
+++ src/main/scala/com/industries/damian/Main.scala
@@ -1,5 +1,7 @@
 package com.industries.damian
 
+import scala.collection.immutable.Map
+
 object Main {
   def top_3_words(str: String): List[String] = {
     str.split("[^a-zA-Z0-9']")
@@ -13,5 +15,54 @@ object Main {
       .sortWith((entry1, entry2) => entry1._2 > entry2._2)
       .take(3)
       .map(entry => entry._1)
+  }
+
+  def top_3_words_mkII(str: String): List[String] = {
+    case class WordAndFreq(word: String, freq: Int)
+    case class TopThreeWords(one: WordAndFreq, two: WordAndFreq, three: WordAndFreq) {
+      def contains(word: String): Boolean = {
+        if (one.word == word ||
+          two.word == word ||
+          three.word == word) true
+        else false
+      }
+
+      private def reindex: TopThreeWords = {
+        val ordered: IndexedSeq[WordAndFreq] = IndexedSeq(one, two, three)
+          .sortWith((wf1, wf2) => wf1.freq > wf2.freq)
+        TopThreeWords(ordered(0), ordered(1), ordered(2))
+      }
+
+      def updated(word: String, wordFreqMap: Map[String, Int]): TopThreeWords = {
+        val freq = wordFreqMap.getOrElse(word, 0)
+        (one, two, three) match {
+          case (wf, _, _) if wf.word == word => TopThreeWords(WordAndFreq(wf.word, one.freq + 1), two, three)
+          case (_, wf, _) if wf.word == word => TopThreeWords(one, WordAndFreq(wf.word, two.freq + 1), three)
+          case (_, _, wf) if wf.word == word => TopThreeWords(one, two, WordAndFreq(wf.word, three.freq + 1))
+          case (_, _, _) if freq > one.freq => TopThreeWords(WordAndFreq(word, freq), one, two)
+          case (_, _, _) if freq > two.freq => TopThreeWords(one, WordAndFreq(word, freq), two)
+          case (_, _, _) if freq > three.freq => TopThreeWords(one, two, WordAndFreq(word, freq))
+          case _ => TopThreeWords(one, two, three)
+        }
+      }.reindex
+    }
+    case class Acc(wordFreqMap: Map[String, Int], top3MostFreqWords: TopThreeWords)
+
+    val noWord = WordAndFreq("", 0)
+
+    val top3w = str.split("[^a-zA-Z0-9']")
+      .withFilter(word => word != "")
+      .map(word => word.toLowerCase)
+      .foldLeft(Acc(Map[String, Int](), TopThreeWords(noWord, noWord, noWord))) { (acc, word) =>
+        val map: Map[String, Int] = acc.wordFreqMap
+        val updatedMap = map.updated(word, map.getOrElse(word, 0) + 1)
+        val top3: TopThreeWords = acc.top3MostFreqWords
+        val updatedTop3 = top3.updated(word, updatedMap)
+        Acc(updatedMap, updatedTop3)
+      }
+      .top3MostFreqWords
+
+    List(top3w.one.word, top3w.two.word, top3w.three.word)
+      .filter(w => w != "")
   }
 }
--- src/test/scala/com/industries/damian/Tests.scala
+++ src/test/scala/com/industries/damian/Tests.scala
@@ -9,11 +9,23 @@ class Tests extends AnyFunSuite {
     assert(top_3_words("In a village of La Mancha, the name of which I have no desire to call to mind, there lived not long since one of those gentlemen that keep a lance in the lance-rack, an old buckler, a lean hack, and a greyhound for coursing. An olla of rather more beef than mutton, a salad on most nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra on Sundays, made away with three-quarters of his income.") == List("a", "of", "on"))
   }
 
-  test("given letter sequences, of mixed case, find tHe top three sequences that obey the word rules") {
+  test("given letter sequences, of mixed case, find the top three sequences that obey the word rules") {
     assert(top_3_words("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e") == List("e", "ddd", "aa"))
   }
 
   test("given different variations of won't, find the top (up to three) valid versions") {
     assert(top_3_words("  //wont won't won't") == List("won't", "wont"))
+  }
+
+  test("given a paragraph, find the top three words MKII") {
+    assert(top_3_words_mkII("In a village of La Mancha, the name of which I have no desire to call to mind, there lived not long since one of those gentlemen that keep a lance in the lance-rack, an old buckler, a lean hack, and a greyhound for coursing. An olla of rather more beef than mutton, a salad on most nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra on Sundays, made away with three-quarters of his income.") == List("a", "of", "on"))
+  }
+
+  test("given letter sequences, of mixed case, find the top three sequences that obey the word rules MKII") {
+    assert(top_3_words_mkII("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e") == List("e", "ddd", "aa"))
+  }
+
+  test("given different variations of won't, find the top (up to three) valid versions MKII") {
+    assert(top_3_words_mkII("  //wont won't won't") == List("won't", "wont"))
   }
 }
